a8d5c5a90b1ca982af2d22d400d058b4aa090084
cttedwards <cttedwards@users.noreply.github.com>
2026-02-03 18:14:36 +0000

    Update version number

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 .onAttach <- function(libname, pkgname) {
-    packageStartupMessage("bdm version 1.0.0")
+    packageStartupMessage("bdm version 1.0.0 (03-Feb-2026)")
 }
-
+ 
 .onLoad <- function(libname, pkgname) {
-    invisible(suppressPackageStartupMessages(
-        sapply(c("methods", "rstan", "StanHeaders", "rlang", "dplyr", "ggplot2"),
-               requireNamespace, quietly = TRUE)
-    ))
+  invisible(suppressPackageStartupMessages(
+    sapply(c("methods", "rstan", "StanHeaders", "rlang", "dplyr", "ggplot2"),
+        requireNamespace, quietly = TRUE)
+  ))
 }
