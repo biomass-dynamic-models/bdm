02b1f10cbf5be79c595e3f0a90b58b9a4cbe221e
cttedwards <cttedwards@users.noreply.github.com>
2026-09-14 13:51:41 +0000

    Update version number

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
-    packageStartupMessage("bdm version 1.0.0 (16-Feb-2026)")
+    packageStartupMessage("bdm version 1.0.0 (14-Sep-2026)")
 }
  
 .onLoad <- function(libname, pkgname) {
